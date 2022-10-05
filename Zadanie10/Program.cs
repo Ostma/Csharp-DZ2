5c19e212f0eac15169efef8b74d6a34ea2f7822c
MaxO <ostma@mail.ru>
2022-10-05 22:09:15 +0500

    Zadanie10

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNumber ()
+{
+Console.WriteLine("Input trehznachnoe:");
+string NumberStr = Console.ReadLine();
+int number = int.Parse(NumberStr);
+return number;
+}
+
+int num = GetNumber();
+
+int seconddig = (num - 100 * (num / 100)) / 10;
+
+
+Console.WriteLine (seconddig);
+
