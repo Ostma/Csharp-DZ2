fe9ce5c06396c0cb8022bbd451b981c2f90e1de4
MaxO <ostma@mail.ru>
2022-10-05 22:45:29 +0500

    Zadanie15

---
+++ Zadanie15/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+//6 -> да
+//7 -> да
+//1 -> нет
+
+int GetNumber()
+{
+    Console.WriteLine("Input den':");
+    string NumberStr = Console.ReadLine();
+    int number = int.Parse(NumberStr);
+    return number;
+}
+
+int num = GetNumber();
+
+if (num == 6 || num == 7)
+    Console.WriteLine("да");
+else
+
+    Console.WriteLine("нет");
